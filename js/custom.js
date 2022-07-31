51cf680949ec63d22985e5f9b04c41701227c4ed
Mahmoud Nofal <33782797+MahmoudNof@users.noreply.github.com>
2022-07-31 09:35:35 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,163 @@
+$(document).ready(function(){
+// start coulmn
+    $(".active").click(function(){
+        $(this).addClass("active").siblings().removeClass("active")
+    })
+    
+    $(".active").click(function(){
+        $(".coulmn").slideDown(1000)
+    })
+
+    $(".active").click(function(){
+        $(".coulmn1").slideUp(1000)
+    })
+
+    $(".active").click(function(){
+        $(".coulmn2").slideUp(1000)
+    })
+
+    $(".active").click(function(){
+        $(".coulmn3").slideUp(1000)
+    })
+
+    $(".active").click(function(){
+        $(".coulmn4").slideUp(1000)
+    })
+
+// end coulmn
+
+// start coulmn1
+
+    $(".one").click(function(){
+        $(this).addClass("active").siblings().removeClass("active")
+    })
+
+    $(".one").click(function(){
+        $(".coulmn1").slideDown(1000)
+    })
+
+    $(".one").click(function(){
+        $(".coulmn1").css("background-color","#3e3e3e")
+    })
+
+    $(".one").click(function(){
+        $(".coulmn").slideUp(1000)
+    })
+
+    $(".one").click(function(){
+        $(".coulmn2").slideUp(1000)
+    })
+
+    $(".one").click(function(){
+        $(".coulmn3").slideUp(1000)
+    })
+
+    $(".one").click(function(){
+        $(".coulmn4").slideUp(1000)
+    })
+    
+// end coulmn1
+
+
+// start coulmn2
+
+    $(".two").click(function(){
+        $(this).addClass("active").siblings().removeClass("active")
+    })
+
+    $(".two").click(function(){
+        $(".coulmn2").slideDown(1000)
+    })
+
+    $(".two").click(function(){
+        $(".coulmn2").css("background-color","#514e86")
+    })
+
+    $(".two").click(function(){
+        $(".coulmn").slideUp(1000)
+    })
+
+    $(".two").click(function(){
+        $(".coulmn1").slideUp(1000)
+    })
+
+    $(".two").click(function(){
+        $(".coulmn3").slideUp(1000)
+    })
+
+    $(".two").click(function(){
+        $(".coulmn4").slideUp(1000)
+    })
+
+// end coulmn2
+
+
+// start coulmn3
+
+
+    $(".three").click(function(){
+        $(this).addClass("active").siblings().removeClass("active")
+    })
+    
+    $(".three").click(function(){
+        $(".coulmn3").slideDown(1000)
+    })
+
+    $(".three").click(function(){
+        $(".coulmn3").css("background-color","#045b66")
+    })
+
+    $(".three").click(function(){
+        $(".coulmn").slideUp(1000)
+    })
+
+    $(".three").click(function(){
+        $(".coulmn1").slideUp(1000)
+    })
+
+    $(".three").click(function(){
+        $(".coulmn2").slideUp(1000)
+    })
+
+    $(".three").click(function(){
+        $(".coulmn4").slideUp(1000)
+    })
+
+// end coulmn3
+
+
+
+// start coulmn4
+
+$(".four").click(function(){
+    $(this).addClass("active").siblings().removeClass("active")
+})
+
+$(".four").click(function(){
+    $(".coulmn4").slideDown(1000)
+})
+
+$(".four").click(function(){
+    $(".coulmn4").css("background-color","#26804e")
+})
+
+$(".four").click(function(){
+    $(".coulmn").slideUp(1000)
+})
+
+$(".four").click(function(){
+    $(".coulmn1").slideUp(1000)
+})
+
+$(".four").click(function(){
+    $(".coulmn2").slideUp(1000)
+})
+
+$(".four").click(function(){
+    $(".coulmn3").slideUp(1000)
+})
+
+// end coulmn4
+
+  
+})
